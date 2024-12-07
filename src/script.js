3d1fc7b966f1c522a41e21d26ba272504614d209
Stergios Fotoglou <stfotoglou@gmail.com>
2024-12-07 15:26:31 +0200

    test

--- src/script.js
+++ src/script.js
@@ -1,11 +1,37 @@
+// Explore button homepage
 document.addEventListener("DOMContentLoaded", () => {
   const titleText = document.querySelector(".title-text");
   const button = document.querySelector(".explore-btn");
 
-  titleText.addEventListener("animationend", (event) => {
-    if (event.animationName === "animated-text") {
-      button.classList.remove("hidden");
-      button.classList.add("visible");
-    }
-  });
+  titleText &&
+    titleText.addEventListener("animationend", (event) => {
+      if (event.animationName === "animated-text") {
+        button.classList.remove("hidden");
+        button.classList.add("visible");
+      }
+    });
+});
+
+// Carousel
+const carouselContainer = document.querySelector(".carousel");
+const carouselItems = document.querySelectorAll(".carousel-item");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+let currentIndex = 0;
+
+carouselItems.forEach((item, index) => {
+  item.style.opacity = index === 0 ? "1" : "0";
+});
+
+prevBtn.addEventListener("click", () => {
+  currentIndex--;
+  carouselItems[currentIndex + 1].style.opacity = "0";
+  carouselItems[currentIndex].style.opacity = "1";
+});
+
+nextBtn.addEventListener("click", () => {
+  currentIndex++;
+  carouselItems[currentIndex - 1].style.opacity = "0";
+  carouselItems[currentIndex].style.opacity = "1";
 });
